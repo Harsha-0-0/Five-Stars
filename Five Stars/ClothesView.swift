a7223a7f9ec8cfc018e2fc05964a58881bf63133
Yi <liyi1997119@gmail.com>
2024-11-19 09:51:34 +1100

    created grid view

---
+++ Five Stars/ClothesView.swift
@@ -0,0 +1,16 @@
+//
+//  ClothesView.swift
+//  Five Stars
+//
+//  Created by Yi Li on 18/11/2024.
+//
+
+import SwiftUI
+
+struct ClothingItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let image: String
+    let description: String 
+}
+
