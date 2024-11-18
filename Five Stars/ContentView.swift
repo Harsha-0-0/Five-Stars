c5c076a385d727d86e2d6118eec1f4eec343e240
Harsha <88568919+Harsha-0-0@users.noreply.github.com>
2024-11-18 11:49:18 +1100

    Add files via upload

---
+++ Five Stars/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Five Stars
+//
+//  Created by Harsha Varthini Maniraj on 18/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, guys!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Five Stars/Five_StarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Five_StarsApp.swift
+//  Five Stars
+//
+//  Created by Harsha Varthini Maniraj on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Five_StarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
